8192b01e1ca4b0f4a6ed86cd08e6723d0da25354
kainel54 <kainel0504@gmail.com>
2025-04-12 14:02:01 +0900

    feat : EntitySystem,Animator,FSM,Input

---
+++ Assets/01Script/PlayerInputSO.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace YH.Players
+{
+    [CreateAssetMenu(fileName = "PlayerInputSO", menuName = "SO/PlayerInputSO")]
+    public class PlayerInputSO : ScriptableObject, Controls.IPlayerActions
+    {
+        public event Action JumpEvent;
+        public event Action DashEvent;
+        public event Action AttackEvent;
+        
+        public Vector2 InputDirection { get; private set; }
+        
+        private Controls _controls;
+
+        private void OnEnable()
+        {
+            if (_controls == null)
+            {
+                _controls= new Controls();
+                _controls.Player.SetCallbacks(this);
+            }
+            _controls.Player.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _controls.Player.Disable();
+        }
+
+    }
+}

7f1b48b1ca8b07dea92b63e2dae77af9855ce88c
kainel54 <kainel0504@gmail.com>
2025-04-24 15:06:45 +0900

    fix : skillmanager

--- Assets/01Script/Core/SkillManager.cs
+++ Assets/01Script/Core/SkillManager.cs
@@ -13,15 +13,13 @@ namespace DKProject.Core
         public event Action OnChangeValue;
         [SerializeField] private SkillListSO _skillList;
         private string fileName = "Skill";
-        public SkillManager()
+
+        protected override void CreateInstance()
         {
+            base.CreateInstance();
             Load();
             skillDictionary = new Dictionary<SkillSO, SkillData>();
             SkillDictionarySet();
-        }
-
-        private void Awake()
-        {
             DontDestroyOnLoad(this.gameObject);
         }
 
